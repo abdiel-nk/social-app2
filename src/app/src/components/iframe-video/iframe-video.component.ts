bf6ab7a3665308a3da05ea55ce520b9f3ae5bd5d
unknown <abdielmamani@upeu.edu.pe>
2024-10-31 20:35:45 -0500

    iframe in progress

--- src/app/src/components/iframe-video/iframe-video.component.ts
+++ src/app/src/components/iframe-video/iframe-video.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+
+
 
 @Component({
   selector: 'app-iframe-video',
   standalone: true,
-  imports: [],
+  imports: [MainContainerComponent],
   templateUrl: './iframe-video.component.html',
   styleUrl: './iframe-video.component.css'
 })
 export class IframeVideoComponent {
+  domeSanitizer = inject(DomSanitizer);
+
+  @Input() set videoSrc(value: string){
+    this._videoSrc = this.domeSanitizer.bypassSecurityTrustResourceUrl(value);
+  }
+
+  _videoSrc: SafeResourceUrl = '';
 
 }
